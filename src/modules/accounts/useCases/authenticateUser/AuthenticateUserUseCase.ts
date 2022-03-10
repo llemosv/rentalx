f8e86d15cbf3b02c088c0271d5500bbf5099ec58
llemosv <leonardo.lemos2002@hotmail.com>
2022-03-10 13:54:37 -0300

    chapter III

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,58 @@
+import { inject, injectable } from "tsyringe";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { AppError } from "../../../../errors/AppError";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError("Email or password incorrect!");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new AppError("Email or password incorrect!");
+    }
+
+    const token = sign({}, "c22b9bd438ee329cba660ae04e2b8c33", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const tokenReturn: IResponse = {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import { ensureAutheticated } from "../middlewares/ensureAuthenticated";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
 const specificationsRoutes = Router();
 const createSpecificationController = new CreateSpecificationController();
+
+specificationsRoutes.use(ensureAutheticated);
 
 specificationsRoutes.post("/", createSpecificationController.handle);
 
